f79c36638ddb25c4dcf6ff4225d2876be0b8a0dc
Ivonne Valenzuela <ivonne@cabreraduncan.com>
2020-08-10 18:44:09 -0500

    Creation products arrays

--- MyFirstComponent/src/app/app.component.ts
+++ MyFirstComponent/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,30 @@ export class AppComponent {
   title = 'MyFirstComponent';
 
   items = ['Juan carlos', 'Juan Jose', 'Ivonne'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'blu blu blu blu'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'blu blu blu blu'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'blu blu blu blu'
+    },
+  ];
 
   addItem() {
     this.items.push('nuevo item');
