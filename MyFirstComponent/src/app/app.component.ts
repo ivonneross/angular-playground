f49e982ff8425961dcbcfaf349b6669c3f09ef05
Ivonne Valenzuela <ivonne@cabreraduncan.com>
2020-08-10 16:54:17 -0500

    Buttons. add and delete

--- MyFirstComponent/src/app/app.component.ts
+++ MyFirstComponent/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyFirstComponent';
+
+  items = ['Juan carlos', 'Juan Jose', 'Ivonne'];
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
